045d1d11d569bf3abe16e4c58a254a3309b25976
Madkour17 <madkour17@hotmail.com>
2022-08-11 22:17:25 +0200

    remove unnecessary constant file, fix types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
-import { nidRegex, Result } from './constants'
 import { cities } from './egCities'
+
+export const nidRegex: RegExp = new RegExp('(2|3)[0-9][1-9][0-1][1-9][0-3][1-9]')
+
+export type Result = {
+    age: number
+    city: string
+    sex: 'Male' | 'Female'
+    day: number
+    month: number
+    year: number
+    bd: string
+}
 /**
  * 
  * @param nid Egyptian national ID - 14 Digits
